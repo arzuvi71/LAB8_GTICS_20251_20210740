b42d337e1aa41a2769d8dfacce957d0f9f8c95d3
Labtel <a20210740@pucp.edu.pe>
2025-05-30 21:31:22 -0500

    first commit

---
+++ src/main/java/com/example/lab6_20210740/Lab620210740Application.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20210740;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab620210740Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab620210740Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab6_20210740/Lab620210740ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20210740;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620210740ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
